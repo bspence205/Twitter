f3a4f2e652544a82b478b6fde2842971654722a7
bspence205 <brianspencer@spencerbrian.com>
2022-06-07 03:40:24 -0700

    created component folder and used rfce to create 2 of the 3 main sections of the app

---
+++ components/Sidebar.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import {
+    BellIcon,
+    HashtagIcon,
+    BookmarkIcon,
+    CollectionIcon,
+    DotsCircleHorizontalIcon,
+    MailIcon,
+    UserIcon,
+    HomeIcon,
+} from '@heroicons/react/outline'
+
+function Sidebar() {
+  return (
+    <div>
+
+        <img className='h-10 w-10' 
+        src="icons8-twitter.svg" alt="twitter logo" />
+    </div>
+  )
+}
+
+export default Sidebar
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Sidebar from '../components/Sidebar'
 
 const Home: NextPage = () => {
   return (
@@ -10,8 +11,13 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Hello World</h1>
+      {/* Sidebar */}
 
+      <Sidebar />
+
+      {/* Feed */}
+
+      {/* Widgets */}
 
     </div>
   )
