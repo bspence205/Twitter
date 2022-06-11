c914ad2e195b1ca9020203c7c1f2f5208fdf86fa
bspence205 <brianspencer@spencerbrian.com>
2022-06-11 15:24:43 -0700

    added twitter and nextAuth keys for api, login functionality enabled via twitter credentials, updated Tweet button to be disabled if not in a session

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -10,8 +10,11 @@ import {
     HomeIcon,
 } from '@heroicons/react/outline'
 import SidebarRow from './SidebarRow'
+import { signIn, signOut, useSession } from 'next-auth/react'
 
 function Sidebar() {
+  const { data: session } = useSession()
+
   return (
     <div className='flex flex-col col-span-2 items-center px-4 md:items-start'>
 
@@ -23,7 +26,8 @@ function Sidebar() {
         <SidebarRow Icon={MailIcon} title="Messages" />
         <SidebarRow Icon={BookmarkIcon} title="Bookmarks" />
         <SidebarRow Icon={CollectionIcon} title="Lists" />
-        <SidebarRow Icon={UserIcon} title="Sign In" />
+        <SidebarRow onClick={session ? signOut : signIn} 
+        Icon={UserIcon} title={session ? 'Sign out': 'Sign In'} />
         <SidebarRow Icon={DotsCircleHorizontalIcon} title="More" />
 
 
--- components/TweetBox.tsx
+++ components/TweetBox.tsx
@@ -7,18 +7,20 @@ import {
     CalendarIcon,
     LocationMarkerIcon
 } from '@heroicons/react/outline' 
+import { useSession } from 'next-auth/react'
 
 
 function TweetBox() {
 
     const [input, setInput] = useState<string>('')
+    const { data: session } = useSession()
 
 
   return (
     <div className='flex space-x-2 p-5'>
         <img
         className='mt-4 h-14 w-14 rounded-full object-cover'
-        src="icons8-name-48.png" alt="" />
+        src={session?.user?.image || "icons8-name-48.png"} alt=""/>
 
         <div className='flex flex-1 items-center pl-2'>
             <form className='flex flex-1 flex-col' action="">
@@ -37,7 +39,8 @@ function TweetBox() {
                         <CalendarIcon className='h-5 w-5' />
                         <LocationMarkerIcon className='h-5 w-5' />
                     </div>
-                    <button disabled={!input} className='bg-twitter px-5 py-2 font-bold
+                    <button disabled={!input || !session} 
+                    className='bg-twitter px-5 py-2 font-bold
                     text-white rounded-full disabled:opacity-40'>Tweet</button>
                 </div>
             </form>
